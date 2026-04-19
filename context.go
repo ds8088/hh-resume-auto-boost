9d25acf448ded164c6bc577caece7e5f0c447b2e
ds8088 <212849436+ds8088@users.noreply.github.com>
2026-04-19 11:34:41 +0300

    chore: remove unused WithCancel method
    This method is unused and unlikely to ever be used so it's only natural to elide it.

--- context.go
+++ context.go
@@ -7,11 +7,3 @@ type AppContext struct {
 	context.Context
 	Cfg Config
 }
-
-func (ac *AppContext) WithCancel() (*AppContext, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(ac)
-	return &AppContext{
-		Context: ctx,
-		Cfg:     ac.Cfg,
-	}, cancel
-}
